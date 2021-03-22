fcc4edac33616f8f2217682ef5abf98f7382edea
User <eponari19@epoka.edu.al>
2021-03-22 20:39:46 +0100

    Added some news

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,10 @@ class MyApp extends StatelessWidget {
             body: Column(
 
               children: [
-                showNews("Higher education crisis", "Higher education still is suffering from remote studying.", 5, 2),
+                showNews("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+
+                showNews("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+                showNews("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
                 showNews("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
               ],
             )));
