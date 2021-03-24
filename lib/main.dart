c76c09c8396f8fe9ac6a378631926def871d7862
User <eponari19@epoka.edu.al>
2021-03-24 16:04:28 +0100

    Added button functionality and routes

--- lib/main.dart
+++ lib/main.dart
@@ -1,32 +1,36 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:like_button/like_button.dart';
+import 'news.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(
+      MaterialApp(
+        title:"Social Albania",
+        home: MyApp()
+      )
+  );
 }
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-        title: 'Social Albania',
-        home: Scaffold(
-            appBar: AppBar(title: Center(child: Text("Social Albania"))),
-            body: Column(
-
+    return Scaffold(
+        appBar: AppBar(title: Center(child: Text("Social Albania"))),
+        body: Column(
               children: [
-                showNews("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
-
-                showNews("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
-                showNews("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
-                showNews("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
+                showNews(context,news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12)),
+                showNews(context,news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12)),
+                showNews(context,news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50)),
+                showNews(context,news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33)),
               ],
-            )));
+        ),
+    );
   }
 
-  Container showNews(String title, String text, int likes,int comments){
+  Container showNews(BuildContext context,news currentNews){
+
     return Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
@@ -34,21 +38,28 @@ class MyApp extends StatelessWidget {
           Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              Text(title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
-              Text(text,style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
+              Text(currentNews.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+              Text(currentNews.description,style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
             ],
           ),
           Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
               Row(children: [
-                LikeButton(),
-                Text(likes.toString(),style: TextStyle(fontSize: 20),)
+                LikeButton(
+                  likeCount: currentNews.likes,
+                ),
               ],),
               Row(
                 children: [
-                  Icon(Icons.add_comment),
-                  Text(comments.toString(),style: TextStyle(fontSize: 20),)
+                  IconButton(
+                    icon: Icon(
+                        Icons.add_comment,
+                    ),
+                    onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>descriptionPage(currentNews)));
+                    },
+                  ),
                 ],
               )
             ],
@@ -56,5 +67,21 @@ class MyApp extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class descriptionPage extends StatelessWidget{
+  news mainNews;
+
+  descriptionPage(news mainNews){
+    this.mainNews=mainNews;
+  }
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+          appBar: AppBar(title: Center(child: Text(mainNews.title))),
+          body: Text(mainNews.description),
+      );
   }
 }
---
+++ lib/news.dart
@@ -0,0 +1,12 @@
+class news{
+  String title,description;
+  int likes,comments;
+
+  news(String title,String description,int likes,int comments){
+    this.title=title;
+    this.description=description;
+    this.likes=likes;
+    this.comments=comments;
+  }
+
+}
