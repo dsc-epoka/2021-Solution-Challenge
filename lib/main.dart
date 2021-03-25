a24af308c3107626e27ced52e3a5903b6727ffef
User <eponari19@epoka.edu.al>
2021-03-25 16:56:43 +0100

    Added button and route to add new articles

--- lib/main.dart
+++ lib/main.dart
@@ -14,17 +14,48 @@ void main() {
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
+
+  List <news> currentNews=[
+    news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+    news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+    news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
+    news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
+    news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+    news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+    news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
+    news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
+    news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+    news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+    news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
+    news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33)
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(title: Center(child: Text("Social Albania"))),
-        body: Column(
-              children: [
-                showNews(context,news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12)),
-                showNews(context,news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12)),
-                showNews(context,news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50)),
-                showNews(context,news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33)),
-              ],
+        body: Padding(
+          padding:  EdgeInsets.all(3.0),
+          child: Stack(
+            children: [
+              SingleChildScrollView(
+                child: Column(
+                      children: [
+                        for(var i in currentNews) showNews(context, i)
+                        ],
+                ),
+              ),
+              Positioned(
+                bottom: 16,
+                right: 16,
+                child: FloatingActionButton(
+                    child: Icon(Icons.add),
+                    onPressed: (){
+                      Navigator.push(context,MaterialPageRoute(builder: (context)=>addingNewsPage()));
+                    }),
+              )
+            ],
+          ),
         ),
     );
   }
@@ -34,14 +65,11 @@ class MyApp extends StatelessWidget {
     return Container(
       child: Row(
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(currentNews.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
-              Text(currentNews.description,style: TextStyle(fontSize: 10,fontWeight: FontWeight.bold),)
-            ],
-          ),
+
+          Text(currentNews.title,style: TextStyle(fontWeight: FontWeight.bold,fontSize: 20)),
+
           Column(
             crossAxisAlignment: CrossAxisAlignment.end,
             children: [
@@ -83,5 +111,36 @@ class descriptionPage extends StatelessWidget{
           appBar: AppBar(title: Center(child: Text(mainNews.title))),
           body: Text(mainNews.description),
       );
+  }
+}
+
+class addingNewsPage extends StatelessWidget{
+  news mainNews;
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: Center(child: Text("Add an article"))),
+      body: Column(
+        children: [
+          TextField(
+            decoration: InputDecoration(
+              hintText: "Enter the title of the article."
+            ),
+            onSubmitted: (val1){
+
+            },
+          ),
+          TextField(
+            decoration: InputDecoration(
+                hintText: "Enter the title of the article."
+            ),
+            onSubmitted: (val1){
+
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
