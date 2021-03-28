a0a560cc4e1c6b1bd5044d4035f853b55ccae797
User <eponari19@epoka.edu.al>
2021-03-28 21:58:41 +0200

    Added refresh feature, added adding news and after refreshing showing them

--- lib/main.dart
+++ lib/main.dart
@@ -12,38 +12,59 @@ void main() {
   );
 }
 
-class MyApp extends StatelessWidget {
+List <news> currentNews=[
+  news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+  news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+  news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
+  news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
+  news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+  news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+  news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
+  news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
+  news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
+  news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
+  news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
+  news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33)
+];
+
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
   // This widget is the root of your application.
 
-  List <news> currentNews=[
-    news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
-    news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
-    news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
-    news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
-    news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
-    news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
-    news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
-    news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33),
-    news("Higher education crisis", "Higher education still is suffering from remote studying.", 15, 12),
-    news("Local Businesses automation", "E-Commerce is getting more attention.", 36, 12),
-    news("Heavy traffic at Tirana's entry point", "Construction of Shqiponja Square set to finish on early 2022.", 42, 50),
-    news("Upcoming elections problems", "Violent conflicts in the streets .", 25, 33)
-  ];
+  Column showedNews;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+
+    void refreshNews(){
+      showedNews= Column(
+        children: [
+          for(var i in currentNews) showNews(context, i)
+        ],
+      );
+    }
+
+    refreshNews();
+
+    return RefreshIndicator(
+      onRefresh: () async {
+        refreshNews();
+        setState(() {
+          
+        });
+      },
+      child: Scaffold(
         appBar: AppBar(title: Center(child: Text("Social Albania"))),
         body: Padding(
           padding:  EdgeInsets.all(3.0),
           child: Stack(
             children: [
               SingleChildScrollView(
-                child: Column(
-                      children: [
-                        for(var i in currentNews) showNews(context, i)
-                        ],
-                ),
+                child: showedNews,
               ),
               Positioned(
                 bottom: 16,
@@ -57,6 +78,7 @@ class MyApp extends StatelessWidget {
             ],
           ),
         ),
+      ),
     );
   }
 
@@ -82,7 +104,7 @@ class MyApp extends StatelessWidget {
                 children: [
                   IconButton(
                     icon: Icon(
-                        Icons.add_comment,
+                      Icons.add_comment,
                     ),
                     onPressed: (){
                       Navigator.push(context,MaterialPageRoute(builder: (context)=>descriptionPage(currentNews)));
@@ -97,6 +119,9 @@ class MyApp extends StatelessWidget {
     );
   }
 }
+
+
+
 
 class descriptionPage extends StatelessWidget{
   news mainNews;
@@ -117,28 +142,48 @@ class descriptionPage extends StatelessWidget{
 class addingNewsPage extends StatelessWidget{
   news mainNews;
 
+  static TextEditingController getTitle= new TextEditingController();
+  static TextEditingController getDescription= new TextEditingController();
+
+  TextField title=TextField(
+    controller: getTitle,
+    decoration: InputDecoration(
+        hintText: "Enter the title of the article."
+    ),
+    onSubmitted: (val1){
+
+    },
+  );
+  TextField description=TextField(
+    controller: getDescription,
+    decoration: InputDecoration(
+        hintText: "Enter the title of the article."
+    ),
+    onSubmitted: (val1){
+
+    },
+  );
+
+
   @override
   Widget build(BuildContext context){
     return Scaffold(
       appBar: AppBar(title: Center(child: Text("Add an article"))),
       body: Column(
         children: [
-          TextField(
-            decoration: InputDecoration(
-              hintText: "Enter the title of the article."
-            ),
-            onSubmitted: (val1){
-
-            },
-          ),
-          TextField(
-            decoration: InputDecoration(
-                hintText: "Enter the title of the article."
-            ),
-            onSubmitted: (val1){
-
-            },
-          ),
+          title,
+          description,
+          Positioned(
+            bottom: 16,
+            right: 16,
+            child: FloatingActionButton(
+                child: Icon(Icons.add),
+                onPressed: (){
+                  news newNews=new news(getTitle.text,getDescription.text,0,0);
+                  currentNews.add(newNews);
+                  Navigator.pop(context);
+                }),
+          )
         ],
       ),
     );
